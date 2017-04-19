59f727e7b264a2518b3cbf28a39816e5eb289e81
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-04-19 17:47:07 +0200

    Sockets for Python are ready: full duplex sockets in two threads

--- Ros-Node/javalinker.py
+++ Ros-Node/javalinker.py
@@ -18,56 +18,74 @@
 
 import socket
 from threading import Thread
+import time
 from SocketServer import ThreadingMixIn
+
+
 # Multithreaded Python server : TCP Server Socket Thread Pool
-# from tcpserver import conn
+
+class ServerThread(Thread):
+    def __init__(self, _IP_, _PORT_, _BUFFER_):
+        print "Initializing serverthread"
+        Thread.__init__(self)
+        self._IP_ = _IP_
+        self._PORT_ = _PORT_
+        self._BUFFER_ = _BUFFER_
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self._IP_, self._PORT_))
+
+    def run(self):
+        print "Starting serverthread"
+        self.server_socket.listen(4)
+        (conn, (ip, port)) = self.server_socket.accept()
+        print "[+] New server socket thread started for " + self._IP_ + ":" + str(self._PORT_)
+        while True:
+            data = conn.recv(2048)
+            print "Server received data:", data
 
 
 class ClientThread(Thread):
-    def __init__(self, ip, port, conn):
+    def __init__(self, _IP_, _PORT_, _BUFFER_):
+        print "Initializing clientthread"
         Thread.__init__(self)
-        self.ip = ip
-        self.port = port
-        self.conn = conn
-        print "[+] New server socket thread started for " + ip + ":" + str(port)
+        self._IP_ = _IP_
+        self._PORT_ = _PORT_
+        self._BUFFER_ = _BUFFER_
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print "Clientthread initialized"
 
     def run(self):
-        while True:
-            data = self.conn.recv(2048)
-            print "Server received data:", data
-            MESSAGE = raw_input("Multithreaded Python server : Enter Response from Server/Enter exit:")
-            if MESSAGE == 'exit':
-                break
-            self.conn.send(MESSAGE)  # echo
+        print "Starting clientthread"
+        self.client_socket.connect((self._IP_, self._PORT_))
+        print "[+] New client socket thread started for " + self._IP_ + ":" + str(self._PORT_)
+
+        i = 0
+        while i < 10:
+            MESSAGE = str(i) +
+            self.client_socket.send(MESSAGE)
+            i += 1
+            time.sleep(1)
+
 
 # Multithreaded Python server : TCP Server Socket Program Stub
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Usually 1024, but we need quick response
-tcpServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpServer.bind((TCP_IP, TCP_PORT))
 
+newthread = ServerThread(TCP_IP, TCP_PORT, BUFFER_SIZE)
+newthread.daemon = True
+newthread.start()
 
-def get_thread_server():
-    tcpServer.listen(4)
-    print "Multithreaded Python server : Waiting for connections from TCP clients..."
-    print "pipi"
-    (conn, (ip, port)) = tcpServer.accept()
-    print "pis"
-    newthread = ClientThread(ip, port, conn)
-    print "kak"
-    newthread.start()
-    print "stront"
-    # return newthread
+time.sleep(1)
 
-server = get_thread_server()
-server.start()
+newthread2 = ClientThread(TCP_IP, TCP_PORT, BUFFER_SIZE)
+newthread2.daemon = True
+newthread2.start()
+newthread2.join()
+print "newthread2 finished"
 
-helloworld = "Hello, World!"
-print helloworld
-tcpServer.send(helloworld)
-answer = tcpServer.recv(BUFFER_SIZE)
-print "I received data: ", answer
-
-server.join()
+while True:
+    time.sleep(1)
