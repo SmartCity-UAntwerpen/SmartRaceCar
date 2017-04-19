19641e92c95aea987740d67be0f055bf6af894c9
Wouter Jansen <wouter.jansen@student.uantwerpen.be>
2017-04-19 21:42:14 +0200

    Updated InputController with working states of driving. TODO: sending this information to Interface Node on vehicle

--- Java-Core/src/JavaCore/InputController.java
+++ Java-Core/src/JavaCore/InputController.java
@@ -8,19 +8,19 @@ import org.jnativehook.keyboard.NativeKeyListener;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class InputController implements NativeKeyListener {
 
-    private final Set<Character> pressed = new HashSet<Character>();
+    private final Set<Integer> pressed = new HashSet<>(); // list of currently pressed buttons
+    int speed = 0; //speed of all wheels
+    int rotation = 0; //rotation of front wheels
+    boolean driving = false; // state of driving
 
+    //action on when key is pressed down
     public void nativeKeyPressed(NativeKeyEvent e) {
-        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-        pressed.add(e.getKeyChar());
-        if (pressed.size() > 1) {
-            // More than one key is currently pressed.
-            // Iterate over pressed to get the keys.
-        }
+        //stop program with ESC
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
             try {
                 GlobalScreen.unregisterNativeHook();
@@ -28,18 +28,97 @@ public class InputController implements NativeKeyListener {
                 e1.printStackTrace();
             }
         }
+        //toggle driving state
+        else if (e.getKeyCode() == NativeKeyEvent.VC_F4) {
+            driving = !driving;
+            System.out.println("driving:" + driving);
+            if(!driving){
+                speed = 0;
+                rotation = 0;
+            }
+        }
+        //safety stop of vehicle when SPACE is pressed
+        else if (e.getKeyCode() == NativeKeyEvent.VC_SPACE) {
+            speed = 0;
+            rotation = 0;
+            driving = false;
+            System.out.println("SAFETY STOP");
+        }else{
+            if((!pressed.contains(e.getKeyCode()))) {
+                pressed.add(e.getKeyCode()); //add key to keylist
+                if(driving)updateStatus();
+            }
+        }
     }
 
+    //action on when key is released
     public void nativeKeyReleased(NativeKeyEvent e) {
-        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-        pressed.remove(e.getKeyChar());
+        pressed.remove(e.getKeyCode()); //remove key from keylist
+        if(driving)updateStatus();
     }
 
+    //update status of wheels based on keylist
+    public void updateStatus(){
+        if(!pressed.contains(44) && !pressed.contains(31)) { // no Z or S
+            speed = 0;
+            rotation = 0;
+        }
+        if(pressed.contains(44)){ // Z
+            if(pressed.contains(16)){ //Z & Q
+                speed = 5;
+                rotation = -50;
+                System.out.println("forward left");
+            }else if(pressed.contains(32)){ //Z & D
+                speed = 5;
+                rotation = 50;
+                System.out.println("forward right");
+            }else if(pressed.contains(31)){ //Z & S
+                speed = 0;
+                rotation = 0;
+                System.out.println("forward backward");
+            }else{ // only Z
+                speed = 10;
+                rotation = 0;
+                System.out.println("forward");
+            }
+        }
+
+        if(pressed.contains(31)){ // S
+            if(pressed.contains(16)){ //S & Q
+                speed = -5;
+                rotation = -50;
+                System.out.println("backward left");
+            }else if(pressed.contains(32)){ //S & D
+                speed = -5;
+                rotation = 50;
+                System.out.println("backward right");
+            }else if(pressed.contains(44)){ //S & Z
+                speed = 0;
+                rotation = 0;
+                System.out.println("backward forward");
+            }else{ // only S
+                speed = -10;
+                rotation = 0;
+                System.out.println("backward");
+            }
+        }
+        sendUpdate();
+    }
+
+    //send current status to car
+    public void sendUpdate(){
+        //TODO
+    }
+
+    //action when key is typed in(virtual keyboards)
     public void nativeKeyTyped(NativeKeyEvent e) {
         System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
     }
 
     public static void main(String[] args) {
+        LogManager.getLogManager().reset();
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
         try {
             GlobalScreen.registerNativeHook();
         }
@@ -47,8 +126,7 @@ public class InputController implements NativeKeyListener {
 
             System.exit(1);
         }
-        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-        logger.setLevel(Level.OFF);
+
         GlobalScreen.addNativeKeyListener(new InputController());
     }
 }
