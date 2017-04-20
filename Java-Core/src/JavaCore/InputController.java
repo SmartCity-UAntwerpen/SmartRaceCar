2f3c438d21ebd702d53992422d3eee82b296d5c4
Wouter Jansen <wouter.jansen@student.uantwerpen.be>
2017-04-20 10:42:29 +0200

    Updated InputController with sending it's status as JSON over TCP socket to Python and having function to get messages back as well.

--- Java-Core/src/JavaCore/InputController.java
+++ Java-Core/src/JavaCore/InputController.java
@@ -4,7 +4,11 @@ import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import org.jnativehook.keyboard.NativeKeyListener;
-
+import org.json.simple.JSONObject;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.logging.Level;
@@ -14,14 +18,25 @@ import java.util.logging.Logger;
 public class InputController implements NativeKeyListener {
 
     private final Set<Integer> pressed = new HashSet<>(); // list of currently pressed buttons
-    int speed = 0; //speed of all wheels
-    int rotation = 0; //rotation of front wheels
+    static int speed = 0; //speed of all wheels
+    static int rotation = 0; //rotation of front wheels
     boolean driving = false; // state of driving
+    static Socket clientSocket = null;
+    static PrintStream os = null;
+    DataInputStream inputLine = null;
+
 
     //action on when key is pressed down
     public void nativeKeyPressed(NativeKeyEvent e) {
         //stop program with ESC
         if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            os.close();
+            try {
+                clientSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            System.out.print("disconnected\n");
             try {
                 GlobalScreen.unregisterNativeHook();
             } catch (NativeHookException e1) {
@@ -46,7 +61,11 @@ public class InputController implements NativeKeyListener {
         }else{
             if((!pressed.contains(e.getKeyCode()))) {
                 pressed.add(e.getKeyCode()); //add key to keylist
-                if(driving)updateStatus();
+                if(driving) try {
+                    updateStatus();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
@@ -54,11 +73,15 @@ public class InputController implements NativeKeyListener {
     //action on when key is released
     public void nativeKeyReleased(NativeKeyEvent e) {
         pressed.remove(e.getKeyCode()); //remove key from keylist
-        if(driving)updateStatus();
+        if(driving) try {
+            updateStatus();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 
     //update status of wheels based on keylist
-    public void updateStatus(){
+    public void updateStatus() throws IOException {
         if(!pressed.contains(44) && !pressed.contains(31)) { // no Z or S
             speed = 0;
             rotation = 0;
@@ -102,12 +125,33 @@ public class InputController implements NativeKeyListener {
                 System.out.println("backward");
             }
         }
-        sendUpdate();
+        JSONObject parentData = new JSONObject();
+        JSONObject childData = new JSONObject();
+        childData.put("steer",rotation);
+        childData.put("throttle", speed);
+        parentData.put("drive",childData);
+        sendUpdate(parentData);
     }
 
-    //send current status to car
-    public void sendUpdate(){
-        //TODO
+    public void sendUpdate(JSONObject json){
+        inputLine = new DataInputStream(new ByteArrayInputStream((json.toString().getBytes(StandardCharsets.UTF_8))));
+        if (clientSocket != null && os != null) {
+            try {
+
+        /*
+         * Keep on reading from/to the socket till we receive the "Ok" from the
+         * server, once we received that then we break.
+         */
+                os.println(inputLine.readLine());
+        /*
+         * Close the output stream, close the input stream, close the socket.
+         */
+            } catch (UnknownHostException e) {
+                System.err.println("Trying to connect to unknown host: " + e);
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
+        }
     }
 
     //action when key is typed in(virtual keyboards)
@@ -115,7 +159,7 @@ public class InputController implements NativeKeyListener {
         System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LogManager.getLogManager().reset();
         Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
         logger.setLevel(Level.OFF);
@@ -125,6 +169,17 @@ public class InputController implements NativeKeyListener {
         catch (NativeHookException ex) {
 
             System.exit(1);
+        }
+        TCPListener tcpListener = new TCPListener();
+        tcpListener.start();
+
+        try {
+            clientSocket = new Socket("localhost", 5005);
+            os = new PrintStream(clientSocket.getOutputStream());
+        } catch (UnknownHostException e) {
+            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println(e);
         }
 
         GlobalScreen.addNativeKeyListener(new InputController());
---
+++ Java-Core/src/JavaCore/TCPListener.java
@@ -0,0 +1,45 @@
+package JavaCore;
+
+import java.io.DataInputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+public class TCPListener extends Thread{
+    public void run(){
+
+        ServerSocket echoServer = null;
+        String line;
+        DataInputStream is;
+        Socket clientSocket;
+
+    /*
+     * Open a server socket on port 2222. Note that we can't choose a port less
+     * than 1023 if we are not privileged users (root).
+     */
+        try {
+            echoServer = new ServerSocket(5006);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    /*
+     * Create a socket object from the ServerSocket to listen to and accept
+     * connections. Open input and output streams.
+     */
+        try {
+            clientSocket = echoServer.accept();
+            is = new DataInputStream(clientSocket.getInputStream());
+
+      /* As long as we receive data, echo that data back to the client. */
+            while (true) {
+                line = is.readLine();
+                if(line != null)System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+}
