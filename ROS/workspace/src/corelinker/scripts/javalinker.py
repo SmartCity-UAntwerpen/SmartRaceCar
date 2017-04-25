9afd316492590276f8599f1d6ab9742ee7348213
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-04-26 00:08:35 +0200

    Deleted publisher part of ‘drive_parameters’ for debugging

--- ROS/workspace/src/corelinker/scripts/javalinker.py
+++ ROS/workspace/src/corelinker/scripts/javalinker.py
@@ -13,8 +13,9 @@ TCP_PORT_JAVA_PYTH = 5005
 TCP_PORT_PYTH_JAVA = 5006
 BUFFER_SIZE = 64
 
-pub_drive_parameters = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# pub_drive_parameters = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
 rospy.init_node('javalinker', anonymous=True)
 
 class ServerThread(Thread):
