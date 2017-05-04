b14a27a194fc819baa652d90d9f2666c834ae115
RaceCar <jens.dehoog@student.uantwerpen.be>
2017-05-04 11:10:38 +0100

    Initial pose will be sent 3 times now

--- ROS/workspace/src/corelinker/scripts/javalinker.py
+++ ROS/workspace/src/corelinker/scripts/javalinker.py
@@ -67,22 +67,25 @@ def stop():
 
 def publish_initialpose(posx, posy, posz, orx, ory, orz, orw):
     if not DEBUG:
-        pose = PoseWithCovarianceStamped()
-        pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = "map"
-        pose.pose.pose.position.x = posx
-        pose.pose.pose.position.y = posy
-        pose.pose.pose.position.z = posz
-        pose.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        i = 0
+        while i < 3:
+            pose = PoseWithCovarianceStamped()
+            pose.header.stamp = rospy.Time.now()
+            pose.header.frame_id = "map"
+            pose.pose.pose.position.x = posx
+            pose.pose.pose.position.y = posy
+            pose.pose.pose.position.z = posz
+            pose.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                                 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
                                 0.06853891945200942]
-        pose.pose.pose.orientation.x = orx
-        pose.pose.pose.orientation.y = ory
-        pose.pose.pose.orientation.z = orz
-        pose.pose.pose.orientation.w = orw
+            pose.pose.pose.orientation.x = orx
+            pose.pose.pose.orientation.y = ory
+            pose.pose.pose.orientation.z = orz
+            pose.pose.pose.orientation.w = orw
 
-        rospy.loginfo(pose)
-        pub_initial_pose.publish(pose)
+            rospy.loginfo(pose)
+            pub_initial_pose.publish(pose)
+            i += 1
         logging.info("Initial pose published!")
     return
 
