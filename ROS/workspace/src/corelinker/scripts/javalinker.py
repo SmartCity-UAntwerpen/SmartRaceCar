27fb749e3f47bf403b76038e4902a8497385e9c7
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-05-11 00:34:14 +0200

    Deleted unnecessary comments

--- ROS/workspace/src/corelinker/scripts/javalinker.py
+++ ROS/workspace/src/corelinker/scripts/javalinker.py
@@ -19,17 +19,6 @@ logger = logger.Logger()
 if not DEBUG_WITHOUT_ROS:
     rosmodule.init_ros(logger)
     logger.log_debug("[JAVALINKER] Debug with ros!")
-    # import rospy
-    # from race.msg import drive_param
-    # from geometry_msgs.msg import PoseWithCovarianceStamped
-    # from geometry_msgs.msg import PoseStamped
-    # from actionlib_msgs.msg import GoalStatusArray
-    # from move_base_msgs.msg import MoveBaseActionFeedback
-    #
-    # pub_drive_parameters = rospy.Publisher('drive_parameters', drive_param, queue_size=10)
-    # pub_initial_pose = rospy.Publisher('initialpose', PoseWithCovarianceStamped, queue_size=10)
-    # pub_move_base_goal = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
-    # rospy.init_node('javalinker', anonymous=True)
 
 TCP_IP = '127.0.0.1'
 TCP_PORT_JAVA_PYTH = 5005
@@ -54,69 +43,6 @@ currentw = 4
 
 cb_movebase_feedback_secs = 0
 
-
-# def publish_drive_param(json_string):
-#     if not DEBUG_WITHOUT_ROS:
-#         msg = drive_param()
-#         msg.steer = json_string['drive']['steer']
-#         msg.throttle = json_string['drive']['throttle']
-#         rospy.loginfo(msg)
-#         pub_drive_parameters.publish(msg)
-#     return
-#
-
-# def stop():
-#     if not DEBUG_WITHOUT_ROS:
-#         msg = drive_param()
-#         msg.steer = 0
-#         msg.throttle = 0
-#         rospy.loginfo(msg)
-#         pub_drive_parameters.publish(msg)
-#     return
-
-
-# def publish_initialpose(posx, posy, posz, orx, ory, orz, orw):
-#     if not DEBUG_WITHOUT_ROS:
-#         i = 0
-#         while i < 3:
-#             pose = PoseWithCovarianceStamped()
-#             pose.header.stamp = rospy.Time.now()
-#             pose.header.frame_id = "map"
-#             pose.pose.pose.position.x = posx
-#             pose.pose.pose.position.y = posy
-#             pose.pose.pose.position.z = posz
-#             pose.pose.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-#                                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-#                                     0.0, 0.0, 0.06853891945200942]
-#             pose.pose.pose.orientation.x = orx
-#             pose.pose.pose.orientation.y = ory
-#             pose.pose.pose.orientation.z = orz
-#             pose.pose.pose.orientation.w = orw
-#
-#             rospy.loginfo(pose)
-#             pub_initial_pose.publish(pose)
-#             i += 1
-#         logging.info("Initial pose published!")
-#     return
-
-
-# def publish_movebase_goal(posx, posy, posz, orx, ory, orz, orw):
-#     if not DEBUG_WITHOUT_ROS:
-#         pose = PoseStamped()
-#         pose.header.stamp = rospy.Time.now()
-#         pose.header.frame_id = "map"
-#         pose.pose.position.x = posx
-#         pose.pose.position.y = posy
-#         pose.pose.position.z = posz
-#         pose.pose.orientation.x = orx
-#         pose.pose.orientation.y = ory
-#         pose.pose.orientation.z = orz
-#         pose.pose.orientation.w = orw
-#
-#         rospy.loginfo(pose)
-#         pub_move_base_goal.publish(pose)
-#         logging.info("Goal published!")
-#     return
 
 
 def read_line(sock, recv_buffer=4096, delim='\n'):
@@ -269,14 +195,6 @@ def cb_movebase_status(data):
     status_list = data.status_list
     if len(status_list) != 0:
         print "[STATUS] Status: %d" % (status_list[len(status_list) - 1].status)
-
-
-# def cb_amcl_pose(data):
-#     pose = data.pose.pose
-#
-#     print "[AMCL] Position: X: %f, Y: %f, Z: %f" % (pose.position.x, pose.position.y, pose.position.z)
-#     print "[AMCL] Orientation: X: %f, Y: %f, Z: %f, W: %f" % (pose.orientation.x, pose.orientation.y, pose.orientation.z
-#                                                               , pose.orientation.w)
 
 
 def cb_movebase_feedback(data):
