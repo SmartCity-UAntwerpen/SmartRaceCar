12cc0e62a2f96d5a1665cf6fd885158440f4c933
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-05-11 00:06:56 +0200

    Deleted ‘global’ statements in functions

--- ROS/workspace/src/corelinker/scripts/handlers/logger.py
+++ ROS/workspace/src/corelinker/scripts/handlers/logger.py
@@ -10,39 +10,29 @@ except ImportError:
 class Logger:
 
     def __init__(self):
-        global debug_without_ros
-        print "logger"
 
         if debug_without_ros:
             logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
     def log_debug(self, text):
-        global debug_without_ros
-
         if debug_without_ros:
             logging.debug(text)
         else:
             rospy.logdebug(text)
 
     def log_info(self, text):
-        global debug_without_ros
-
         if debug_without_ros:
             logging.info(text)
         else:
             rospy.loginfo(text)
 
     def log_warning(self, text):
-        global debug_without_ros
-
         if debug_without_ros:
             logging.warning(text)
         else:
             rospy.logwarn(text)
 
     def log_critical(self, text):
-        global debug_without_ros
-
         if debug_without_ros:
             logging.critical(text)
         else:
