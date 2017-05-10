e1ec887a2e9d9a1335fd441872ce9add44ebbac2
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-05-10 23:41:29 +0200

    Made class of Logger-module + the module detects whether ‘rospy’ is available for logging functionality

--- ROS/workspace/src/corelinker/scripts/handlers/logger.py
+++ ROS/workspace/src/corelinker/scripts/handlers/logger.py
@@ -1,21 +1,49 @@
 import logging
 
-
-def init_logger():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
-
-
-def log_debug(text):
-    logging.debug(text)
+try:
+    import rospy
+    debug_without_ros = False
+except ImportError:
+    debug_without_ros = True
 
 
-def log_info(text):
-    logging.info(text)
+class Logger:
 
+    def __init__(self):
+        global debug_without_ros
+        print "logger"
 
-def log_warning(text):
-    logging.warning(text)
+        if debug_without_ros:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
+    def log_debug(self, text):
+        global debug_without_ros
 
-def log_critical(text):
-    logging.critical(text)
+        if debug_without_ros:
+            logging.debug(text)
+        else:
+            rospy.logdebug(text)
+
+    def log_info(self, text):
+        global debug_without_ros
+
+        if debug_without_ros:
+            logging.info(text)
+        else:
+            rospy.loginfo(text)
+
+    def log_warning(self, text):
+        global debug_without_ros
+
+        if debug_without_ros:
+            logging.warning(text)
+        else:
+            rospy.logwarn(text)
+
+    def log_critical(self, text):
+        global debug_without_ros
+
+        if debug_without_ros:
+            logging.critical(text)
+        else:
+            rospy.logerr(text)
