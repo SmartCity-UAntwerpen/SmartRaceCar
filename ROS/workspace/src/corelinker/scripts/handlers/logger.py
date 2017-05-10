efbd8621a124e921b647f2254155c87f0be69522
Jens de Hoog <jens.dehoog@student.uantwerpen.be>
2017-05-11 00:07:32 +0200

    Added adjustable logging level

--- ROS/workspace/src/corelinker/scripts/handlers/logger.py
+++ ROS/workspace/src/corelinker/scripts/handlers/logger.py
@@ -6,13 +6,30 @@ try:
 except ImportError:
     debug_without_ros = True
 
+'''
+Logging level:
+    1: DEBUG
+    2: INFO
+    3: WARNING
+    4: CRITICAL
+'''
+logging_level = 2
+
 
 class Logger:
 
     def __init__(self):
+        self.logging_level = logging_level
 
         if debug_without_ros:
-            logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+            if self.logging_level == 1:
+                logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
+            elif self.logging_level == 2:
+                logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+            elif self.logging_level == 3:
+                logging.basicConfig(level=logging.WARNING, format='%(asctime)s [%(levelname)s] %(message)s')
+            elif self.logging_level == 4:
+                logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s [%(levelname)s] %(message)s')
 
     def log_debug(self, text):
         if debug_without_ros:
