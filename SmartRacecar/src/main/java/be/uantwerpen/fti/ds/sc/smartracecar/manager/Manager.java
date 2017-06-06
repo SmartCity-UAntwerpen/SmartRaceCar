fa7685a6db89ead0b74624c099a4dd7c3aa12f19
Wouter Jansen <wouter.jansen@student.uantwerpen.be>
2017-06-06 15:38:43 +0200

    Added correct response messages for jobExecute

--- SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/manager/Manager.java
+++ SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/manager/Manager.java
@@ -24,7 +24,7 @@ import java.util.List;
 import java.util.logging.Level;
 
 @Path("carmanager")
-public class Manager implements MQTTListener{
+public class Manager implements MQTTListener {
 
     private boolean debugWithoutBackBone = true; // debug parameter to stop attempts to send or recieve messages from backbone.
     private boolean debugWithoutMAAS = true; // debug parameter to stop attempts to send or recieve messages from MAAS
@@ -34,7 +34,7 @@ public class Manager implements MQTTListener{
     private final String mqqtUsername = "root";
     private final String mqttPassword = "smartcity";
     private final String restURLMAAS = "http://localhost:8080/";
-    private final String restURLBackBone = "http://146.175.140.44:1994/";
+    private final String restURLBackBone = "http://143.129.39.151:10000/";
 
     private static MQTTUtils mqttUtils;
     private static RESTUtils restUtilsMAAS;
@@ -51,11 +51,11 @@ public class Manager implements MQTTListener{
 
     }
 
-    public Manager(String currentMap,String mapsPath) throws MqttException, IOException {
+    public Manager(String currentMap, String mapsPath) throws MqttException, IOException {
         log = new Log(this.getClass(), level);
         Manager.currentMap = currentMap;
         Manager.mapsPath = mapsPath;
-        log.logConfig("MANAGER","Startup parameters: Map: " + currentMap + " | Path to maps folder: " + mapsPath );
+        log.logConfig("MANAGER", "Startup parameters: Map: " + currentMap + " | Path to maps folder: " + mapsPath);
 
         restUtilsMAAS = new RESTUtils(restURLMAAS);
         restUtilsBackBone = new RESTUtils(restURLBackBone);
@@ -64,20 +64,21 @@ public class Manager implements MQTTListener{
         mqttUtils.subscribeToTopic("racecar/#");
     }
 
-    private void loadWayPoints(){
-        if(debugWithoutBackBone){
-            wayPoints.put((long)8,new WayPoint(8,(float)0.5,(float)0,(float)-1,(float)0.02));
-            wayPoints.put((long)9,new WayPoint(9,(float)-13.4,(float)-0.53,(float)0.71,(float)0.71));
-            wayPoints.put((long)10,new WayPoint(10,(float)-27.14,(float)-1.11,(float)-0.3,(float)0.95));
-            wayPoints.put((long)11,new WayPoint(11,(float)-28.25,(float)-9.19,(float)-0.71,(float)0.71));
-        }else{
+    private void loadWayPoints() {
+        if (debugWithoutBackBone) {
+            wayPoints.put((long) 8, new WayPoint(8, (float) 0.5, (float) 0, (float) -1, (float) 0.02));
+            wayPoints.put((long) 9, new WayPoint(9, (float) -13.4, (float) -0.53, (float) 0.71, (float) 0.71));
+            wayPoints.put((long) 10, new WayPoint(10, (float) -27.14, (float) -1.11, (float) -0.3, (float) 0.95));
+            wayPoints.put((long) 11, new WayPoint(11, (float) -28.25, (float) -9.19, (float) -0.71, (float) 0.71));
+        } else {
             String jsonString = restUtilsBackBone.getJSON("map/stringmapjson/car");
             JSONUtils.isJSONValid(jsonString);
-            Type typeOfWayPointArray = new TypeToken<ArrayList<WayPoint>>() {}.getType();
+            Type typeOfWayPointArray = new TypeToken<ArrayList<WayPoint>>() {
+            }.getType();
             ArrayList<WayPoint> wayPointsTemp = (ArrayList<WayPoint>) JSONUtils.getObject(jsonString, typeOfWayPointArray);
             for (WayPoint wayPoint : wayPointsTemp) {
-                wayPoints.put(wayPoint.getID(),wayPoint);
-                Log.logConfig("MANAGER","Added wayPoint with ID " + wayPoint.getID() + " and coordinates " + wayPoint.getX() +"," + wayPoint.getY() +"," + wayPoint.getZ() +"," + wayPoint.getW() +".");
+                wayPoints.put(wayPoint.getID(), wayPoint);
+                Log.logConfig("MANAGER", "Added wayPoint with ID " + wayPoint.getID() + " and coordinates " + wayPoint.getX() + "," + wayPoint.getY() + "," + wayPoint.getZ() + "," + wayPoint.getW() + ".");
             }
         }
         Log.logInfo("CORE", "All possible waypoints(" + wayPoints.size() + ") received.");
@@ -92,18 +93,17 @@ public class Manager implements MQTTListener{
             } else {
                 Log.logConfig("MANAGER", "Vehicle with ID " + ID + " doesn't exist. Cant update route information.");
             }
-        }
-        else if (topic.matches("racecar/[0-9]+/percentage")) {
+        } else if (topic.matches("racecar/[0-9]+/percentage")) {
             long ID = Long.parseLong(topic.replaceAll("\\D+", ""));
             if (vehicles.containsKey(ID)) {
-                Type typeOfLocation = new TypeToken<Location>() {}.getType();
-                Location location = (Location) JSONUtils.getObject(message,typeOfLocation);
+                Type typeOfLocation = new TypeToken<Location>() {
+                }.getType();
+                Location location = (Location) JSONUtils.getObject(message, typeOfLocation);
                 vehicles.get(ID).getLocation().setPercentage(location.getPercentage());
             } else {
                 Log.logConfig("MANAGER", "Vehicle with ID " + ID + " doesn't exist. Cant update route percentage.");
             }
-        }
-        else if (topic.matches("racecar/[0-9]+/costanswer")) {
+        } else if (topic.matches("racecar/[0-9]+/costanswer")) {
             long ID = Long.parseLong(topic.replaceAll("\\D+", ""));
             if (vehicles.containsKey(ID)) {
                 Type typeOfCost = new TypeToken<Cost>() {
@@ -112,12 +112,11 @@ public class Manager implements MQTTListener{
             } else {
                 Log.logConfig("MANAGER", "Vehicle with ID " + ID + " doesn't exist. Cannot process cost answer.");
             }
-        }
-        else if (topic.matches("racecar/[0-9]+/available")) {
+        } else if (topic.matches("racecar/[0-9]+/available")) {
             long ID = Long.parseLong(topic.replaceAll("\\D+", ""));
             if (vehicles.containsKey(ID)) {
                 vehicles.get(ID).setAvailable(Boolean.parseBoolean(message));
-                if(vehicles.get(ID).isAvailable())
+                if (vehicles.get(ID).isAvailable())
                     Log.logInfo("MANAGER", "Vehicle with ID " + ID + " set to be available.");
                 else
                     Log.logInfo("MANAGER", "Vehicle with ID " + ID + " set to be no longer available.");
@@ -180,10 +179,10 @@ public class Manager implements MQTTListener{
     public Response getPositions(@Context HttpServletResponse response) throws IOException {
         if (vehicles.isEmpty()) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "no vehicles registered yet");
-        }else{
+        } else {
             List<Location> locations = new ArrayList<>();
             for (Vehicle vehicle : vehicles.values()) {
-                if(vehicle.isAvailable()){
+                if (vehicle.isAvailable()) {
                     locations.add(vehicle.getLocation());
                 }
             }
@@ -199,7 +198,7 @@ public class Manager implements MQTTListener{
     @GET
     @Path("calcWeight/{idStart}/{idStop}")
     @Produces("application/json")
-    public Response calculateCostsRequest(@PathParam("idStart") long startId,@PathParam("idStop") long endId, @Context HttpServletResponse response) throws IOException, InterruptedException {
+    public Response calculateCostsRequest(@PathParam("idStart") long startId, @PathParam("idStop") long endId, @Context HttpServletResponse response) throws IOException, InterruptedException {
         if (!wayPoints.containsKey(startId) || !wayPoints.containsKey(endId)) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "start or end waypoint not found");
         } else if (vehicles.isEmpty()) {
@@ -207,17 +206,17 @@ public class Manager implements MQTTListener{
         } else {
             int totalVehicles = 0;
             for (Vehicle vehicle : vehicles.values()) {
-                if(vehicle.isAvailable()){
+                if (vehicle.isAvailable()) {
                     totalVehicles++;
                     mqttUtils.publishMessage("racecar/" + Long.toString(vehicle.getID()) + "/costrequest", Long.toString(startId) + " " + Long.toString(endId));
                 }
             }
 
-            while(costs.size() != totalVehicles){
+            while (costs.size() != totalVehicles) {
                 Log.logInfo("MANAGER", "waiting for vehicles to complete request.");
                 Thread.sleep(200);
             }
-            ArrayList<Cost> costCopy = (ArrayList<Cost>)costs.clone();
+            ArrayList<Cost> costCopy = (ArrayList<Cost>) costs.clone();
             this.costs.clear();
             Log.logInfo("MANAGER", "Cost calculation request completed.");
             return Response.status(Response.Status.OK).
@@ -238,14 +237,14 @@ public class Manager implements MQTTListener{
     @GET
     @Path("delete/{id}")
     @Produces("text/plain")
-    public Response deleteVehicle(@PathParam("id") final long id,@Context HttpServletResponse response) throws IOException {
+    public Response deleteVehicle(@PathParam("id") final long id, @Context HttpServletResponse response) throws IOException {
         if (vehicles.containsKey(id)) {
-            if(!debugWithoutBackBone)restUtilsBackBone.getTextPlain("delete/" + id);
+            if (!debugWithoutBackBone) restUtilsBackBone.getTextPlain("delete/" + id);
             vehicles.remove(id);
             Log.logInfo("MANAGER", "Vehicle with ID " + id + " stopped. ID removed.");
             return Response.status(Response.Status.OK).build();
-        }else{
-            response.sendError(HttpServletResponse.SC_NOT_FOUND," vehicle with id " +  id + "  not found");
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, " vehicle with id " + id + "  not found");
         }
         return null;
     }
@@ -303,30 +302,34 @@ public class Manager implements MQTTListener{
     @GET
     @Path("executeJob/{idJob}/{idVehicle}/{idStart}/{idEnd}")
     @Produces("text/plain")
-    public Response jobRequest(@PathParam("idJob") long idJob,@PathParam("idVehicle") long idVehicle,@PathParam("idStart") long idStart,@PathParam("idEnd") long idEnd,String data,@Context HttpServletResponse response) throws IOException {
-        Job job = new Job(idJob,idStart,idEnd,idVehicle);
+    public Response jobRequest(@PathParam("idJob") long idJob, @PathParam("idVehicle") long idVehicle, @PathParam("idStart") long idStart, @PathParam("idEnd") long idEnd, String data, @Context HttpServletResponse response) throws IOException {
+        Job job = new Job(idJob, idStart, idEnd, idVehicle);
         if (vehicles.containsKey(job.getIdVehicle())) {
             if (!vehicles.get(job.getIdVehicle()).getOccupied() && vehicles.get(job.getIdVehicle()).isAvailable()) {
-                if(wayPoints.containsKey(job.getIdStart()) && wayPoints.containsKey(job.getIdEnd())){
-                    Location location = vehicles.get(job.getIdVehicle()).getLocation();
-                    location.setIdStart(job.getIdStart());
-                    location.setIdEnd(job.getIdEnd());
-                    location.setPercentage(0);
-                    vehicles.get(job.getIdVehicle()).setLocation(location);
-                    jobSend(job.getIdVehicle(),job.getIdStart(),job.getIdEnd());
-                    return Response.status(Response.Status.OK).build();
-                }else{
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND, "waypoints " + job.getIdStart() + " and/or " + job.getIdEnd() + "not found.");
-                    Log.logWarning("MANAGER", "Can't send route job as waypoints " + job.getIdStart() + " and/or " + job.getIdEnd() + " were not found.");
+                if (wayPoints.containsKey(job.getIdStart())) {
+                    if (wayPoints.containsKey(job.getIdEnd())) {
+                        Location location = vehicles.get(job.getIdVehicle()).getLocation();
+                        location.setIdStart(job.getIdStart());
+                        location.setIdEnd(job.getIdEnd());
+                        location.setPercentage(0);
+                        vehicles.get(job.getIdVehicle()).setLocation(location);
+                        jobSend(job.getIdVehicle(), job.getIdStart(), job.getIdEnd());
+                        return Response.status(Response.Status.OK).build();
+                    } else {
+                        response.sendError(HttpServletResponse.SC_NOT_FOUND,"idEndError");
+                        Log.logWarning("MANAGER", "Can't send route job as waypoints " + job.getIdStart() + " was not found.");
+                    }
+                } else {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND,"idStartError");
+                    Log.logWarning("MANAGER", "Can't send route job as waypoints " + job.getIdStart() + " was not found.");
                 }
-
             } else {
-                response.sendError(HttpServletResponse.SC_NOT_FOUND, "Vehicle with ID " + job.getIdVehicle() + " is occupied or not available.");
+                response.sendError(HttpServletResponse.SC_NOT_FOUND, "VehicleBusy");
                 Log.logWarning("MANAGER", "Vehicle with ID " + job.getIdVehicle() + " is occupied or not available. Cant send route job.");
             }
 
         } else {
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Vehicle with ID " + job.getIdVehicle() + " not found.");
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "idVehicleError");
             Log.logWarning("MANAGER", "Vehicle with ID " + job.getIdVehicle() + " doesn't exist. Cant send route job.");
         }
         return Response.status(Response.Status.OK).build();
@@ -343,8 +346,8 @@ public class Manager implements MQTTListener{
             System.out.println("Need 2 arguments to run. Possible arguments: currentMap(String) mapsPath(String)");
             System.exit(0);
         } else if (args.length == 2) {
-                final Manager manager = new Manager(args[0],args[1]);
-                new TomCatLauncher().start();
+            final Manager manager = new Manager(args[0], args[1]);
+            new TomCatLauncher().start();
 
         }
     }
