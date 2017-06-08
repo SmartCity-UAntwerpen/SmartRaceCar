ab9d24d8b5cd8d6f182402095a368ea2a17004d3
Wouter Jansen <wouter.jansen@student.uantwerpen.be>
2017-06-08 15:25:14 +0200

    Comments and rebuild.

--- SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/common/FileUtils.java
+++ SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/common/FileUtils.java
@@ -4,23 +4,48 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Help class to look for specific files.
+ */
 public class FileUtils {
 
-    private String fileNameToSearch;
-    private List<String> result = new ArrayList<String>();
+    private String fileNameToSearch; // Name of the file that is being searched.
+    private List<String> result = new ArrayList<String>(); // List of all found files matching the searched files. Full path is stored.
 
+    /**
+       /* Get the name of the file being searched.
+       *
+       * @return String with the name of the file being searched.
+       */
     public String getFileNameToSearch() {
         return fileNameToSearch;
     }
 
+    /**
+     /* Get the name of the file being searched.
+     *
+     * @param fileNameToSearch The String with the name of the file being searched.
+     */
     public void setFileNameToSearch(String fileNameToSearch) {
         this.fileNameToSearch = fileNameToSearch;
     }
 
+    /**
+     /* Get the list will all found files.
+     *
+     * @return List with all found files matching the searched filename. Full path.
+     */
     public List<String> getResult() {
         return result;
     }
 
+    /**
+     /* Execute a search in given directory to search.
+     *  It will loop through all subdirectories.
+     *
+     * @param directory Path of the directory to be searched.
+     * @param fileNameToSearch Name of the file to be searched.
+     */
     public void searchDirectory(File directory, String fileNameToSearch) {
 
         setFileNameToSearch(fileNameToSearch);
@@ -28,11 +53,16 @@ public class FileUtils {
         if (directory.isDirectory()) {
             search(directory);
         } else {
-            Log.logSevere("FILESEARCH","Not a directory. Cannot search.");
+            Log.logSevere("FILESEARCH", "Not a directory. Cannot search.");
         }
 
     }
 
+    /**
+     /* Search in given directory for the searched file.
+     *
+     * @param file Path of the directory to be searched.
+     */
     private void search(File file) {
 
         if (file.isDirectory()) {
@@ -51,7 +81,7 @@ public class FileUtils {
                 }
 
             } else {
-                Log.logSevere("FILESEARCH","No permission to search for files.");
+                Log.logSevere("FILESEARCH", "No permission to search for files.");
             }
         }
 
--- SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/common/JSONUtils.java
+++ SmartRacecar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/common/JSONUtils.java
@@ -7,15 +7,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-//Help functions to deal with JSON messages. Uses Google GSON library.
+/**
+ * Help functions to deal with JSON messages. Uses Google GSON library.
+ */
 public class JSONUtils {
 
-    private static final Gson gson = new Gson();
+    private static final Gson gson = new Gson(); // GSON object to use the library functions.
 
-    public JSONUtils(){
-    }
-
-    //Method to verify if string is a valid JSON string.
+    /**
+     * Method to verify if string is a valid JSON string.
+     *
+     * @param jsonInString String to be verified.
+     * @return boolean to state if the string is valid JSON. True means valid.
+     */
     public static boolean isJSONValid(String jsonInString) {
         try {
             gson.fromJson(jsonInString, Object.class);
@@ -26,7 +30,12 @@ public class JSONUtils {
         }
     }
 
-    //Get the first word of the JSON object. In this case this is always the keyword used in the message.
+     /**
+     * Get the first word of the JSON object. In this case this is always the keyword used in the message.
+     *
+     * @param jsonInString String to be searched for the keyword.
+     * @return String that the holds the found keyword.
+     */
     public static String getFirst(String jsonInString) {
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(jsonInString);
@@ -38,8 +47,15 @@ public class JSONUtils {
         return jsonInString;
     }
 
-    //Get the object after the keyword of the JSON object. This is always the package of the message.
-    //Converts it to the correct requested Java Object type.
+
+    /**
+     * Get the object after the keyword of the JSON object. This is always the package of the message.
+     * Converts it to the correct requested Java Object type.
+     *
+     * @param jsonInString String to be searched for the object.
+     * @param t Type of the object being searched.
+     * @return Object that was requested
+     */
     public static Object getObjectWithKeyWord(String jsonInString, Type t){
         JsonParser parser = new JsonParser();
         JsonElement element = parser.parse(jsonInString);
@@ -51,34 +67,70 @@ public class JSONUtils {
         return null;
     }
 
+    /**
+     * Get the object held in the JSON object.
+     * Converts it to the correct requested Java Object type.
+     *
+     * @param jsonInString String to be searched for the object.
+     * @param t Type of the object being searched.
+     * @return Object that was requested
+     */
     public static Object getObject(String jsonInString,Type t){
         return gson.fromJson(jsonInString, t);
     }
 
-    //Converts a Java object of a specific type to a JSON string with a keyword as first element.
+    /**
+     * Converts a Java object of a specific type to a JSON string with a keyword as first element.
+     *
+     * @param keyword Keyword to be used at start of the JSON string to identify the string.
+     * @param object Object to be parsed.
+     * @return Coverted JSON string containing the keyword and the converted object.
+     */
     public static String objectToJSONStringWithKeyWord(String keyword, Object object){
         JsonObject jsonObject = new JsonObject();
         jsonObject.add(keyword,gson.toJsonTree(object));
         return jsonObject.toString();
     }
 
-    //Converts a Java object of a specific type to a JSON string with a keyword as first element.
+    /**
+     * Converts a Java list of a specific type to a JSON string with a keyword as first element.
+     *
+     * @param keyword Keyword to be used at start of the JSON string to identify the string.
+     * @param arrayList List to be parsed.
+     * @return Coverted JSON string containing the keyword and the converted list.
+     */
     public static String arrayToJSONStringWithKeyWord(String keyword, List<?> arrayList){
         JsonObject jsonObject = new JsonObject();
         jsonObject.add(keyword,gson.toJsonTree(arrayList));
         return jsonObject.toString();
     }
 
-    //Converts a Java object of a specific type to a JSON string with a keyword as first element.
+    /**
+     * Converts a Java object of a specific type to a JSON string.
+     *
+     * @param object Object to be parsed.
+     * @return Coverted JSON string containing the converted object.
+     */
     public static String objectToJSONString(Object object){
         return gson.toJson(object);
     }
 
+    /**
+     * Converts a Java list object of a specific type to a JSON string.
+     *
+     * @param arrayList List to be parsed.
+     * @return Coverted JSON string containing the converted list.
+     */
     public static String arrayToJSONString(List<?> arrayList){
         return gson.toJson(arrayList);
     }
 
-    //Converts only a keyword to a JSON string.
+    /**
+     * Converts only a keyword to a JSON string.
+     *
+     * @param keyword to be parsed.
+     * @return Converted keyword to JSON String.
+     */
     public static String keywordToJSONString(String keyword){
         JsonObject parentData = new JsonObject();
         JsonObject childData = new JsonObject();
