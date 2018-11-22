57199c7ae6716f2fc688bef4dadae16ea32931ea
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-11-22 16:02:22 +0100

    Further work on SimKernel cleanup.

---
+++ SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/VehicleManager.java
@@ -0,0 +1,13 @@
+package be.uantwerpen.fti.ds.sc.smartracecar.racecarBackend;
+
+import java.util.HashMap;
+
+public class VehicleManager
+{
+    private HashMap<Integer, Vehicle> vehicles;
+
+    public VehicleManager()
+    {
+
+    }
+}
