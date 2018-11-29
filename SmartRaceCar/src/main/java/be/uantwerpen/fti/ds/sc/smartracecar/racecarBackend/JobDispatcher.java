63c4ad68ace76276582d5722b22f26aa10be975a
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-11-29 14:20:30 +0100

    Finished /job REST endpoint in Job Dispatcher.

--- SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/JobDispatcher.java
+++ SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/JobDispatcher.java
@@ -1,6 +1,6 @@
 package be.uantwerpen.fti.ds.sc.smartracecar.racecarBackend;
 
-import be.uantwerpen.fti.ds.sc.smartracecar.common.LogbackWrapper;
+import be.uantwerpen.fti.ds.sc.smartracecar.common.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -11,17 +11,19 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
-public class JobDispatcher
+public class JobDispatcher implements MQTTListener//todo: Get rid of this, still needed because MQTTUtils will crash if you don't provide it with a listener
 {
     private LogbackWrapper log;
     private MapManager mapManager;
     private VehicleManager vehicleManager;
+    private MQTTUtils mqttUtils;
 
-    public JobDispatcher(MapManager mapManager, VehicleManager vehicleManager)
+    public JobDispatcher(Parameters parameters, MapManager mapManager, VehicleManager vehicleManager)
     {
         this.log = new LogbackWrapper();
         this.mapManager = mapManager;
         this.vehicleManager = vehicleManager;
+        this.mqttUtils = new MQTTUtils(parameters.getMqttBroker(), parameters.getMqttUserName(), parameters.getMqttPassword(), this);
     }
 
     @GET
@@ -31,6 +33,7 @@ public class JobDispatcher
     {
         Job job = new Job(jobId, startId, endId, vehicleId);
 
+        // Check if vehicle exists
         if (!this.vehicleManager.exists(vehicleId))
         {
             String errorString = "Tried to execute job on non-existent vehicle (" + Long.toString(vehicleId) + ")";
@@ -40,6 +43,7 @@ public class JobDispatcher
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
 
+        // Check if vehicle is occupied
         if (this.vehicleManager.get(vehicleId).getOccupied())
         {
             String errorString = "Vehicle " + Long.toString(vehicleId) + " is currently occupied.";
@@ -49,6 +53,7 @@ public class JobDispatcher
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
+        // Check if vehicle is available
         if (!this.vehicleManager.get(vehicleId).isAvailable())
         {
             String errorString = "Vehicle " + Long.toString(vehicleId) + " is currently not available.";
@@ -58,6 +63,7 @@ public class JobDispatcher
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
+        // Check if starting waypoint exists
         if(!this.mapManager.exists(startId))
         {
             String errorString = "Request job with non-existent start waypoint " + Long.toString(startId) + ".";
@@ -67,6 +73,39 @@ public class JobDispatcher
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
+        // Check if end waypoint exists
+        if(!this.mapManager.exists(endId))
+        {
+            String errorString = "Request job with non-existent end waypoint " + Long.toString(startId) + ".";
+            this.log.error("JOB-DISPATCHER", errorString);
+
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, errorString);
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        Vehicle vehicle = this.vehicleManager.get(vehicleId);
+        Location location = new Location(vehicleId, startId, endId, 0);
+
+        vehicle.setJob(job);
+        vehicle.setLocation(location);
+        vehicle.setOccupied(true);
+
+        this.mqttUtils.publishMessage("racecar/" + Long.toString(vehicleId) + "/job", Long.toString(startId) + " " + Long.toString(endId));
+
         return Response.status(Response.Status.OK).build();
+    }
+
+    /**
+     * Dummy MQTT Parsing method.
+     * We only need MQTT Utils to publish, we don't sub to anything.
+     * The MQTTUtils still need a listener because...
+     * Honestly, I don't know why, I just know that it will crash if you don't give it one.
+     * I know, I know ...
+     * @param topic   received MQTT topic
+     * @param message received MQTT message string
+     */
+    @Override
+    public void parseMQTT(String topic, String message)
+    {
     }
 }
