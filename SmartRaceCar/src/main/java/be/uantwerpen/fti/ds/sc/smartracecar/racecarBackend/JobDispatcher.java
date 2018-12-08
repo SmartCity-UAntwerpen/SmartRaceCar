6269da2bf7ec5507fd68e15a924f0db12b54cf2f
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-12-08 17:02:39 +0100

    Cleaning up some unused imports. Deprecating Tomcat related classes.

--- SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/JobDispatcher.java
+++ SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/JobDispatcher.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
-
 @Controller
 public class JobDispatcher implements MQTTListener//todo: Get rid of this, still needed because MQTTUtils will crash if you don't provide it with a listener
 {
