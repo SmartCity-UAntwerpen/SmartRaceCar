7edbcfa04dab756697ec8f00f0ee4653b82377f5
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-12-09 11:10:27 +0100

    Further cleanup in HeartBeatChecker.

--- SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/HeartbeatChecker.java
+++ SmartRaceCar/src/main/java/be/uantwerpen/fti/ds/sc/smartracecar/racecarBackend/HeartbeatChecker.java
@@ -2,6 +2,8 @@ package be.uantwerpen.fti.ds.sc.smartracecar.racecarBackend;
 
 import be.uantwerpen.fti.ds.sc.smartracecar.common.*;
 import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,30 +16,31 @@ import java.util.HashMap;
  * Helper class to check the heartbeat of all registered vehicles periodically
  */
 @Component
-class HeartbeatChecker extends Thread
+class HeartbeatChecker
 {
-	private LogbackWrapper log;
+	private Logger log;
 	private RESTUtils restUtils;
 
 	/**
 	 * constructor for the HeartbeatChecker class
 	 *
-	 * @param url RestURL of the backend object
+	 * @param parameters parameters used to start backend
 	 */
 	@Autowired
 	public HeartbeatChecker(Parameters parameters)
 	{
-		this.log = new LogbackWrapper(HeartbeatChecker.class);
+		this.log = LoggerFactory.getLogger(HeartbeatChecker.class);
 
-		this.log.info("HEARTBEAT-CHECKER", "Creating REST Utils for \"" + parameters.getRESTCarmanagerURL() + "\"...");
+		this.log.info("Creating REST Utils for \"" + parameters.getRESTCarmanagerURL() + "\"...");
 
 		this.restUtils = new RESTUtils(parameters.getRESTCarmanagerURL());
 	}
 
-	@Scheduled(fixedRate = 5000)
+	@Scheduled(fixedRate = 30000)
 	private void checkBeats()
 	{
-		this.log.info("HEARTBEAT-CHECKER", "Heartbeats are being checked...");
+		this.log.info("Heartbeats are being checked...");
+
 		Type typeOfHashMap = new TypeToken<HashMap<Long, Vehicle>>()
 		{
 		}.getType();
@@ -51,41 +54,6 @@ class HeartbeatChecker extends Thread
 				this.log.warning("HEARTBEAT-CHECKER", "Vehicle with ID: " + ID + " was removed since it hasn't responded for over 90s");
 			}
 		}
-		this.log.info("HEARTBEAT-CHECKER", "All heartbeats were checked.");
-	}
-
-	/**
-	 * start the Checker thread
-	 */
-	@Deprecated
-	public void run()
-	{
-		this.log.info("HEARTBEAT-CHECKER", "Heartbeatchecker was started");
-		while (true)
-		{
-			try
-			{
-				Thread.sleep(30000); //Sleep for 30s
-				this.log.info("HEARTBEAT-CHECKER", "Heartbeats are being checked...");
-				Type typeOfHashMap = new TypeToken<HashMap<Long, Vehicle>>()
-				{
-				}.getType();
-				HashMap<Long, Vehicle> vehicles = (HashMap<Long, Vehicle>) JSONUtils.getObjectWithKeyWord(restUtils.getJSON("getVehicles"), typeOfHashMap);
-				Date currentTime = new Date();
-				for (Long ID : vehicles.keySet())
-				{
-					if ((currentTime.getTime() - vehicles.get(ID).getHeartbeat().getTime()) > 90000) //longer than 90 seconds
-					{
-						restUtils.getCall("delete/" + ID);
-						this.log.warning("HEARTBEAT-CHECKER", "Vehicle with ID: " + ID + " was removed since it hasn't responded for over 90s");
-					}
-				}
-				this.log.info("HEARTBEAT-CHECKER", "All heartbeats were checked.");
-
-			} catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-		}
+		this.log.info("All heartbeats were checked.");
 	}
 }
