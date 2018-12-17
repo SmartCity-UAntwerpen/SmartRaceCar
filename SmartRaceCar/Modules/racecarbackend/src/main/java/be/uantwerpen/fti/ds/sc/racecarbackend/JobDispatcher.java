45fd78a4be33cc318eaa97b84cbefc4f58bb1088
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-12-17 11:34:49 +0100

    Further work on new REST endpoints. Cleanup to make code more Java-like rather than using REST calls to contact itself.

--- SmartRaceCar/Modules/racecarbackend/src/main/java/be/uantwerpen/fti/ds/sc/racecarbackend/JobDispatcher.java
+++ SmartRaceCar/Modules/racecarbackend/src/main/java/be/uantwerpen/fti/ds/sc/racecarbackend/JobDispatcher.java
@@ -23,20 +23,25 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 {
 	private Logger log;
 	private JobDispatcherParameters jobDispatcherParameters;
+	private JobTracker jobTracker;
 	private MapManager mapManager;
 	private VehicleManager vehicleManager;
+	private NavigationManager navigationManager;
 	private MQTTUtils mqttUtils;
 
 	@Autowired
-	public JobDispatcher(JobDispatcherParameters jobDispatcherParameters, MapManager mapManager, VehicleManager vehicleManager)
+	public JobDispatcher(JobDispatcherParameters jobDispatcherParameters, JobTracker jobTracker, MapManager mapManager, VehicleManager vehicleManager, NavigationManager navigationManager)
 	{
 		this.log = LoggerFactory.getLogger(this.getClass());
 		this.jobDispatcherParameters = jobDispatcherParameters;
+		this.jobTracker = jobTracker;
 		this.mapManager = mapManager;
 		this.vehicleManager = vehicleManager;
+		this.navigationManager = navigationManager;
 		this.mqttUtils = new MQTTUtils(jobDispatcherParameters.getMqttBroker(), jobDispatcherParameters.getMqttUserName(), jobDispatcherParameters.getMqttPassword(), this);
 	}
 
+	/*
 	@Deprecated
 	@RequestMapping(value = "/carmanager/executeJob/{jobId}/{vehicleId}/{startId}/{endId}", method=RequestMethod.GET, produces=MediaType.TEXT_PLAIN)
 	public @ResponseBody ResponseEntity<String> jobRequest(@PathVariable long jobId, @PathVariable long vehicleId, @PathVariable long startId, @PathVariable long endId)
@@ -44,7 +49,7 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 		Job job = new Job(jobId, startId, endId, vehicleId);
 
 		// Check if vehicle exists
-		if (!this.vehicleManager.exists(vehicleId))
+		if (!this.vehicleManager.existsOld(vehicleId))
 		{
 			String errorString = "Tried to execute job on non-existent vehicle (" + vehicleId + ")";
 			this.log.error(errorString);
@@ -91,7 +96,7 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 		this.log.info("Received Job request for " + vehicleId + " from " + startId + " to " + endId + " (JobID: " + jobId + ")");
 
 		Vehicle vehicle = this.vehicleManager.get(vehicleId);
-		Location location = new Location(vehicleId, startId, endId, 0);
+		Location location = new Location(vehicleId, startId, endId);
 
 		vehicle.setJob(job);
 		vehicle.setLocation(location);
@@ -101,9 +106,10 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
+	*/
 
 	/**
-	 * REST Endpoint used to checkt the cost between the two points.
+	 * REST Endpoint used to check the cost between the two points.
 	 * The cost is calculated on the ROS navstack server (Usually at smartcity.ddns.net:8084)
 	 * If no cars are available, a cost of infinity (Integer.MAX_VALUE) is returned
 	 * @param startId
@@ -113,27 +119,21 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 	@RequestMapping(value="/{startId}/{endId}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON)
 	public @ResponseBody ResponseEntity<String> costRequest(@PathVariable long startId, @PathVariable long endId)
 	{
-		RESTUtils racecarAPI = new RESTUtils(this.jobDispatcherParameters.getRESTCarmanagerURL());
-
 		this.log.info("Received cost request for " + startId + " -> " + endId);
 
-		Boolean startExists = Boolean.parseBoolean(racecarAPI.getTextPlain("/exists/" + startId));
-		Boolean endExists = Boolean.parseBoolean(racecarAPI.getTextPlain("/exists/" + endId));
-		int numVehicles = Integer.parseInt(racecarAPI.getTextPlain("/getNumVehicles"));
-
-		if (!startExists)
+		if (!this.mapManager.existsOld(startId))
 		{
 			this.log.error("Requested cost for start waypoint " + startId + ", but waypoint doesn't exist.");
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 
-		if (!endExists)
+		if (!this.mapManager.existsOld(endId))
 		{
 			this.log.error("Requested cost for end waypoint " + startId + ", but waypoint doesn't exist.");
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 
-		if (numVehicles == 0)
+		if (this.vehicleManager.getNumVehicles() == 0)
 		{
 			this.log.error("Requested cost, but no vehicles are available, returning " + Integer.MAX_VALUE);
 			String responseJson = JSONUtils.objectToJSONStringWithKeyWord("cost", Integer.MAX_VALUE);
@@ -146,11 +146,10 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 		{
 			RESTUtils ROSAPI = new RESTUtils(this.jobDispatcherParameters.getROSServerURL());
 
-			Type pointType = new TypeToken<Point>(){}.getType();
 			Type costType = new TypeToken<Cost>(){}.getType();
 
-			Point startPoint = (Point) JSONUtils.getObject(racecarAPI.getJSON("/getCoordinates/" + startId), pointType);
-			Point endPoint = (Point) JSONUtils.getObject(racecarAPI.getJSON("/getCoordinates/" + endId), pointType);
+			Point startPoint = this.mapManager.getCoordinates(startId);
+			Point endPoint = this.mapManager.getCoordinates(endId);
 
 			List<Point> points = new ArrayList<>();
 			points.add(startPoint);
@@ -168,6 +167,71 @@ public class JobDispatcher implements MQTTListener//todo: Get rid of this, still
 
 		String responseJson = JSONUtils.objectToJSONStringWithKeyWord("cost", cost);
 		return new ResponseEntity<>(responseJson, HttpStatus.OK);
+	}
+
+	@RequestMapping(value="/job/execute/{startId}/{endId}/{jobId}", method=RequestMethod.POST, produces=MediaType.TEXT_PLAIN)
+	public @ResponseBody ResponseEntity<String> executeJob(@PathVariable long startId, @PathVariable long endId, @PathVariable long jobId)
+	{
+		this.log.info("Received Job request for " + startId + " -> " + endId + " (JobID: " + jobId + ")");
+
+		//todo: Replace this with actual resource management
+		long dummyVehicleId = 0;
+		long vehicleId = dummyVehicleId;
+
+		//todo: move to resource manager
+		/*
+		// Check if vehicle exists
+		if (!this.vehicleManager.existsOld(vehicleId))
+		{
+			String errorString = "Tried to execute job on non-existent vehicle (" + vehicleId + ")";
+			this.log.error(errorString);
+
+			return new ResponseEntity<>(errorString, HttpStatus.BAD_REQUEST);
+		}
+
+		// Check if vehicle is occupied
+		if (this.vehicleManager.get(vehicleId).getOccupied())
+		{
+			String errorString = "Vehicle " + vehicleId + " is currently occupied and can't accept any job requests.";
+			this.log.error(errorString);
+
+			return new ResponseEntity<>(errorString, HttpStatus.NOT_FOUND);
+		}
+		// Check if vehicle is available
+		if (!this.vehicleManager.get(vehicleId).isAvailable())
+		{
+			String errorString = "Vehicle " + vehicleId + " is currently not available for job requests.";
+			this.log.error(errorString);
+
+			return new ResponseEntity<>(errorString, HttpStatus.NOT_FOUND);
+		}*/
+
+		RESTUtils racecarAPI = new RESTUtils(this.jobDispatcherParameters.getRESTCarmanagerURL());
+
+		// Check if starting waypoint exists
+		if (!this.mapManager.existsOld(startId))
+		{
+			String errorString = "Request job with non-existent start waypoint " + startId + ".";
+			this.log.error(errorString);
+
+			return new ResponseEntity<>(errorString, HttpStatus.NOT_FOUND);
+		}
+
+		// Check if end waypoint exists
+		if (!this.mapManager.existsOld(endId))
+		{
+			String errorString = "Request job with non-existent end waypoint " + endId + ".";
+			this.log.error(errorString);
+
+			return new ResponseEntity<>(errorString, HttpStatus.NOT_FOUND);
+		}
+
+		this.navigationManager.setLocation(vehicleId, startId);
+		this.vehicleManager.setOccupied(vehicleId, true);
+		this.jobTracker.addJob(jobId, vehicleId, startId, endId);
+
+		this.mqttUtils.publishMessage("racecar/" + vehicleId + "/job", startId + " " + endId);
+		return new ResponseEntity<>("starting", HttpStatus.OK);
 	}
 
 	/**
