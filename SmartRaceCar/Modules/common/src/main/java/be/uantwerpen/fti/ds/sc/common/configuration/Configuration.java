269ff6964ab1ec33f92fa38047334fb8012af997
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-12-24 17:16:30 +0100

    First version of Aspect-based configuration.

---
+++ SmartRaceCar/Modules/common/src/main/java/be/uantwerpen/fti/ds/sc/common/configuration/Configuration.java
@@ -0,0 +1,48 @@
+package be.uantwerpen.fti.ds.sc.common.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Configuration
+{
+	private Logger log;
+	private Map<AspectType, Aspect> aspects;
+
+	public Configuration ()
+	{
+		this.log = LoggerFactory.getLogger(Configuration.class);
+		this.aspects = new HashMap<>();
+	}
+
+	public void add(AspectType aspectType)
+	{
+		this.aspects.put(aspectType, null);
+	}
+
+	public void load(String configurationFilename)
+	{
+		File configFile = new File(configurationFilename);
+
+		for (AspectType type: this.aspects.keySet())
+		{
+			try
+			{
+				switch (type)
+				{
+					case MQTT:
+						this.aspects.put(type, new MqttAspect(configFile));
+						continue;
+				}
+			}
+			catch (IOException ioe)
+			{
+				this.log.error("Failed to load configuration for aspect \"" + type + "\".", ioe);
+			}
+		}
+	}
+}
