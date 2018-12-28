357cebfcf6bbd038d63d973c4a0ee81dd2a78f77
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-12-28 16:47:24 +0100

    Bugfix in SimDeployer Logging config. Started working on better Docker configuration.

--- SmartRaceCar/Modules/common/src/main/java/be/uantwerpen/fti/ds/sc/common/configuration/DockerAspect.java
+++ SmartRaceCar/Modules/common/src/main/java/be/uantwerpen/fti/ds/sc/common/configuration/DockerAspect.java
@@ -12,12 +12,15 @@ public class DockerAspect extends Aspect
 	private static final String PREFIX = "Docker";
 
 	private static final String DOCKER_IMAGE_NAME_KEY = PREFIX + ".image";
-	private static final String[] KEYS = {DOCKER_IMAGE_NAME_KEY};
+	private static final String DOCKER_HOST_VOLUME_KEY = PREFIX + ".volume.host";
+	private static final String[] KEYS = {DOCKER_IMAGE_NAME_KEY, DOCKER_HOST_VOLUME_KEY};
 
 	private static final String DEFAULT_DOCKER_IMAGE_NAME = "astridvanneste/core_simkernel";
+	private static final String DEFAULT_DOCKER_VOLUME = "./docker/config";
 
 	private Logger log;
 	private String imageName;
+	private String volume;
 
 	public DockerAspect (File configFile) throws IOException
 	{
@@ -31,8 +34,10 @@ public class DockerAspect extends Aspect
 			this.checkKeys(properties, KEYS);
 
 			this.imageName = properties.getProperty(DOCKER_IMAGE_NAME_KEY, DEFAULT_DOCKER_IMAGE_NAME);
+			this.volume = properties.getProperty(DOCKER_HOST_VOLUME_KEY, DEFAULT_DOCKER_VOLUME);
 
 			this.log.debug(DOCKER_IMAGE_NAME_KEY + " = " + this.imageName);
+			this.log.debug(DOCKER_HOST_VOLUME_KEY + " = " + this.volume);
 		}
 		catch (IOException ioe)
 		{
@@ -41,13 +46,15 @@ public class DockerAspect extends Aspect
 		}
 	}
 
-	public DockerAspect (String imageName)
+	public DockerAspect (String imageName, String volume)
 	{
 		super(AspectType.DOCKER);
 		this.log = LoggerFactory.getLogger(RacecarAspect.class);
 
 		this.imageName = imageName;
+		this.volume = volume;
 		this.log.debug(DOCKER_IMAGE_NAME_KEY + " = " + this.imageName);
+		this.log.debug(DOCKER_HOST_VOLUME_KEY + " = " + this.volume);
 	}
 
 	public String getImageName()
--- SmartRaceCar/Modules/simdeployerinterface/src/main/java/be/uantwerpen/fti/ds/sc/simdeployerinterface/CommandLineInterface.java
+++ SmartRaceCar/Modules/simdeployerinterface/src/main/java/be/uantwerpen/fti/ds/sc/simdeployerinterface/CommandLineInterface.java
@@ -33,17 +33,17 @@ public class CommandLineInterface
 		Map<String, String> helpMap = new TreeMap<>();  // TreeMap is used to sort commands alphabetically.
 
 		// Add some extra space after each command to make them all the same length
-		helpMap.put("create          SIM_ID       ", "Create a simulation instance. This just announces the ID of the simulation to the SimDeployer.");
-		helpMap.put("set    PROPERTY SIM_ID VALUE ", "Set a parameter of the simulation. Currently only \"startpoint\" is supported. \"speed\" and \"name\" will be parsed correctly, but don't have any effect.");
-		helpMap.put("run             SIM_ID       ", "Run a simulation instance created using the \"create\" command. Requires that the simulation has a start point. This starts an actual Docker container.");
-		helpMap.put("stop            SIM_ID       ", "Kill a simulation instance. The corresponding Docker process will be killed and the simulation will lose any held state.");
-		helpMap.put("kill            SIM_ID       ", "Remove a simulation instance from the list of IDs.");
-		helpMap.put("restart         SIM_ID       ", "Same as run.");
+		helpMap.put("create        SIM_ID         ", "Create a simulation instance. This just announces the ID of the simulation to the SimDeployer.");
+		helpMap.put("set    SIM_ID PROPERTY VALUE ", "Set a parameter of the simulation. Currently only \"startpoint\" is supported. \"speed\" and \"name\" will be parsed correctly, but don't have any effect.");
+		helpMap.put("run           SIM_ID         ", "Run a simulation instance created using the \"create\" command. Requires that the simulation has a start point. This starts an actual Docker container.");
+		helpMap.put("stop          SIM_ID         ", "Kill a simulation instance. The corresponding Docker process will be killed and the simulation will lose any held state.");
+		helpMap.put("kill          SIM_ID         ", "Remove a simulation instance from the list of IDs.");
+		helpMap.put("restart       SIM_ID         ", "Same as run.");
 		helpMap.put("ping                         ", "Used by Simulation front end to keep an eye on SimDeployers. Returns \"pong\".");
 		helpMap.put("help                         ", "Show this help message.");
 		helpMap.put("quit                         ", "Kill the interactive console session. (Leaves the SimDeployer running as normal, doesn't kill any simulations.)");
-		helpMap.put("echo                   STRING", "Print a given string. Multi-word strings should be properly quoted, escaped quotes are reduced to normal quotes.");
-		helpMap.put("wait                   TIME  ", "Wait the specified number of seconds.");
+		helpMap.put("echo                 STRING  ", "Print a given string. Multi-word strings should be properly quoted, escaped quotes are reduced to normal quotes.");
+		helpMap.put("wait                 TIME    ", "Wait the specified number of seconds.");
 
 		StringBuilder helpBuilder = new StringBuilder();
 
