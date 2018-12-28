0fc6c633c3cc47c43e7d9ba88641cd27d7cc284f
Astrid <astrid.vanneste1@gmail.com>
2018-12-28 14:43:33 +0100

    core_simkernel_rosserver with java core and simkernel

--- Docker/Core_SimKernel_ROSServer/run.sh
+++ Docker/Core_SimKernel_ROSServer/run.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+echo $@
+START_POINT=$1
+PORT_1=$2
+PORT_2=$3
+SIM_ID=$4
+
 cd /home/docker/ROS/workspace
 
 source devel/setup.bash
 
-roslaunch rossimulator rossim.launch
+roslaunch rossimulator rossim.launch &
+
+cd /home/docker
+
+nohup java -jar SimKernel.jar ${PORT_2} ${PORT_1} ${SIM_ID} &
+java -jar Core.jar ${START_POINT} ${PORT_1} ${PORT_2}
