6ba2af328ffb86e6ebc2b978ced8d7dca3ac1aba
Astrid <astrid.vanneste1@gmail.com>
2018-12-28 10:58:37 +0100

    changes to python to work in docker

--- ROS/workspace/src/rossimulator/scripts/launch_python.sh
+++ ROS/workspace/src/rossimulator/scripts/launch_python.sh
@@ -3,5 +3,5 @@
 # This script launches the REST-server, which is a python script without ROS
 
 sleep 4
-python3 /home/docker/ROS/workspace/src/rossimulator/scripts/restserver.py
+python /home/docker/ROS/workspace/src/rossimulator/scripts/restserver.py
 
